c0460df2b6cc59d5846347a8e1d5abcdbbf3ef0a
Steven McCann <stevenmancans@gmail.com>
2020-02-11 19:39:29 +1100

    added height and width of squares in createGrid func and based on a x sized grid

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const container = document.querySelector('#container');
+// create a grid with x dimension
+function createGrid(x){
 
-// create a grid 16x16
-function createGrid(){
-    for (i = 0; i < 16; i++) {
-        for (j = 0; j < 16; j++) {
-            const rowSquare = document.createElement('div');
-            rowSquare.classList.add('rowSquare');
-            container.appendChild(rowSquare);
+    const container = document.querySelector('#container');
+    height = container.clientHeight;
+    width = container.clientWidth;
+
+    for (i = 0; i < x; i++) {
+        // create the grids row
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('gridRow');
+
+        for (j = 0; j < x; j++) {
+            // create each square and set width & height
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.height = (height / x).toString() + 'px'
+            square.style.width = (width / x).toString() + 'px'
+            gridRow.appendChild(square);
+            
         }
+        // after each iteration, append the row
+        container.appendChild(gridRow);
     }
+    
 }
 
-createGrid()
+createGrid(16)
