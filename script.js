814141a500b074be405e3348239d5a48ed2e9bae
Steven McCann <stevenmancans@gmail.com>
2020-02-12 09:14:24 +1100

    added functionality to reset grid button

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 // create a grid with x dimension
 function createGrid(x){
-
     const container = document.querySelector('#container');
     height = container.clientHeight;
     width = container.clientWidth;
-
-    for (i = 0; i < x; i++) {
-        // create the grids row
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('gridRow');
-
+    for (i = 0; i < x; i++) {        
         for (j = 0; j < x; j++) {
             // create each square and set width & height
             const square = document.createElement('div');
             square.classList.add('square');
             square.style.height = (height / x).toString() + 'px'
             square.style.width = (width / x).toString() + 'px'
-            gridRow.appendChild(square);
-            
+            container.appendChild(square);
         }
-        // after each iteration, append the row
-        container.appendChild(gridRow);
     }
-    
 }
+
+// check for a mouseover event and change the background color when you get it
+function isMousingOver(){
+    let tileArray = document.getElementsByClassName("square");
+    for (i = 0; i < tileArray.length; i++) {
+        tileArray[i].addEventListener('mouseover', changeBackgroundColor);
+    }
+};
 
 function changeBackgroundColor() {
     this.style.backgroundColor = 'black';
 };
 
+function resetListener(){
+    document.getElementById("reset").addEventListener("click", function(){
+        dimension = prompt('What dimensions would you like your new grid to be?')
+        clearGrid();
+        createGrid(dimension);
+        isMousingOver();
+    });
+}
 
+function clearGrid() {
+    container = document.querySelector('#container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 createGrid(16);
-
-// check for a mouseover event and change the background color when you get it
-let tileArray = document.getElementsByClassName("square");
-
-for (i = 0; i < tileArray.length; i++) {
-    tileArray[i].addEventListener('mouseover', changeBackgroundColor);
-}
+isMousingOver();
+resetListener();
